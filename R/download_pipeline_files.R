4c93d0cbec8e4d4938476659597b0896f63aebda
Nana Mensah <Nana.mensah@crick.ac.uk>
2022-12-07 20:44:07 +0000

    Add function to get pipeline files

---
+++ R/download_pipeline_files.R
@@ -0,0 +1,47 @@
+
+#' Download CAMDAC pipeline files
+#' 
+#' @description CAMDAC pipeline files are required for analysis. This function downloads the files to
+#' the output directory and unpacks them. By default, CAMDAC searches for the files in the
+#' environment variable CAMDAC_PIPELINE_FILES. If this is missing, the current directory is used.
+#' @param assay Sequencing assay. Either wgbs or rrbs.
+#' @param directory Optional. Directory to download files to.
+#' @export
+download_pipeline_files <- function(bsseq, directory=NULL){
+  stopifnot(bsseq %in% c("wgbs", "rrbs", "test"))
+  
+  # Get download URL from CAMDAC index file
+  url_index_file = system.file("extdata", "pipeline_files_urls.txt", package = "CAMDAC")
+  urls = read.table(url_index_file, header=F, stringsAsFactors = F)
+  names(urls) = c("bsseq", "link")
+  link = urls[ urls$bsseq == bsseq, ][[2]]
+  
+  # Get download location
+  #   If a directory is passed to the function, install there.
+  if(!is.null(directory)){
+    location=fs::path_expand(directory)
+    fs::dir_create(location) # Ensure location exists
+  } else {
+  #   Else, get pipeline files location from environment variable
+  #   The currect directory is used if environment variable is empty
+    cpf_env = Sys.getenv("CAMDAC_PIPELINE_FILES")
+    location = ifelse(cpf_env=="", ".", cpf_env)
+    location = fs::path_expand(location)
+  }
+  
+  # Ensure download directory path exists
+  if(!fs::dir_exists(location)){
+    fs::dir_create(location)
+  }
+  
+  # Download pipeline files and unzip
+  tf = tempfile()
+  download.file(link, destfile=tf, method="wget")
+  untar(tf, exdir=location)
+  
+  loginfo("Tempfile unpacked and deleted %s", tf)
+  fs::file_delete(tf)
+  
+  loginfo("Pipeline files for %s downloaded to %s", bsseq, location)
+
+}
