981ecb4724d9e628af01689108b3cb0845efced8
Nana Mensah <Nana.mensah@crick.ac.uk>
2022-12-11 16:44:24 +0000

    Add panel pseudocode

--- R/camdac_wgbs_panel.R
+++ R/camdac_wgbs_panel.R
@@ -6,11 +6,29 @@
 #' @param max_sd Maximum standard deviation of methylation for a site to be included in panel
 #' @param drop_snps Boolean. If TRUE, drop per-sample CG-SNPs (BAF < 0.1 or BAF > 0.9) from panel
 #' @export
-panel_meth_from_counts <- function(
-  ac_files, min_coverage = 3, min_samples = 1, max_sd = 0.1, drop_snps = FALSE
-  ) {
-  x = data.table(total_counts_m=100)
-  x[, `:=`("chrom"=1, "start"=2, "end"=3, "M_n"=10, "UM_n"=10, "m_n"=10, "cov_n"=5)]
+panel_meth_from_counts <- function(ac_files, min_coverage = 3, min_samples = 1,
+                                   max_sd = 0.1, drop_snps = FALSE) {
+  # Load AC files
+
+  # Find unique CpG positions
+
+  # Order AC files by unique CG overlap
+
+  # Filter CpGs by minimum sample coverage threshold
+
+  # Mask sample CpGs by coverage
+
+  # Mask sample CpGs by SNP loci if required
+
+  # Filter CpGs by maximum SD threshold
+
+  # Combine counts to create methylation panel
+
+  # Return panel object
+
+  # Test pipeline:
+  # load_all();test_active_file("tests/testthat/test-panel.R")
+  x <- data.table(total_counts_m = 100)
+  x[, `:=`("chrom" = 1, "start" = 2, "end" = 3, "M_n" = 10, "UM_n" = 10, "m_n" = 10, "cov_n" = 5)]
   return(x)
 }
-
