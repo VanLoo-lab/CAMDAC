28a412a9aaa455b0654c161239b59b172fdf746c
Nana Mensah <Nana.mensah@crick.ac.uk>
2023-01-07 16:47:27 +0000

    Add features to use CNA solutions from external sources

--- R/pipelines.R
+++ R/pipelines.R
@@ -1,34 +1,40 @@
 #' CAMDAC tumor-normal pipeline
-#' 
+#'
 #' Run CAMDAC analysis on a bulk tumor and patient-matched tissue-matched tumor-adjacent normal sample.
-#' 
+#'
 #' @param tumor. Tumor sample data built with `create_camdac_sample()`.
 #' @param normal. Normal sample data built with `create_camdac_sample()`.
 #' @param config. Configuration built with `create_camdac_config()`.
 #' @export
-pipeline_tumor_normal <- function(tumor, normal, config){
-  
+pipeline_tumor_normal <- function(tumor, normal, config) {
   # Log
   loginfo("CAMDAC:::pipeline_tumor_normal start for %s", tumor$patient_id)
-  
+
   # Count SNP and CpG alleles
   cmain_count_alleles(tumor, config)
   cmain_count_alleles(normal, config)
-  
-  # Call copy number abberations using battenberg
-  cmain_make_snp_profiles(tumor, normal, config)
-  cmain_run_battenberg(tumor, normal, config)
-  
+
+  # TODO: Refactor into a CMAIN function that accepts battenberg or ascat as input
+  # If no allele-specific segments are given, call them
+  # Or refit if cna object contains purity and ploidy values alone
+  if (is.null(tumor$cna$ascna)) {
+    # Call copy number abberations using battenberg
+    cmain_make_snp_profiles(tumor, normal, config)
+    cmain_run_battenberg(tumor, normal, config)
+    tumor <- load_cna_data(tumor, config, "battenberg")
+  } else {
+    loginfo("Using external CNA data provided for %s", tumor$patient_id)
+  }
+
   # Deconvolve methylation rates
   cmain_make_methylation_profile(tumor, config)
   cmain_make_methylation_profile(normal, config)
   cmain_deconvolve_methylation(tumor, normal, config)
-  
+
   # Call differential methylation
   cmain_call_dmps(tumor, normal, config)
   cmain_call_dmrs(tumor, config)
 
   # Log
   loginfo("CAMDAC:::pipeline_tumor_normal complete for %s", tumor$patient_id)
-  
 }
---
+++ tests/testthat/test-external-cna.R
@@ -0,0 +1,56 @@
+test_that("CAMDAC deconvolves using pre-computed CNA data", {
+  # Load external CNA object
+  cna <- readRDS(system.file("testdata", "test_cna.rds", package = "CAMDAC"))
+  segments_bed <- system.file("testdata", "test_wgbs_small_segments.bed", package = "CAMDAC")
+
+  # Warning: Currently requires 30 minutes to run and CAMDAC_PIPELINE_FILES environment var
+  #    to be set to the location of the CAMDAC pipeline files.
+  config <- create_camdac_config(
+    outdir = "./result_test", # Path for outputs
+    bsseq = "wgbs", # WGBS
+    build = "hg38", # Reference
+    bsseq_lib = "pe", # Paired end
+    n_cores = 10 # Cores for parallel processing
+  )
+
+  # Get test BAM
+  tumor_bam <- system.file("extdata", "NA18939_test_v3.bam", package = "CAMDAC")
+  normal_bam <- system.file("extdata", "NA20502_test_v3.bam", package = "CAMDAC")
+
+  # Set CAMDAC tumor sample
+  tumor <- create_camdac_sample(
+    patient_id = "P1",
+    patient_sex = "XX",
+    sample_id = "T",
+    sample_type = "tumour",
+    bam_file = tumor_bam,
+    segments_bed = segments_bed,
+    cna = cna
+  )
+
+  # Set CAMDAC normal sample
+  normal <- create_camdac_sample(
+    patient_id = "P1",
+    patient_sex = "XX",
+    sample_id = "N",
+    sample_type = "normal",
+    bam_file = normal_bam,
+    segments_bed = segments_bed
+  )
+
+  # Run pipeline
+  stdout <- testthat::capture_output(
+    pipeline_tumor_normal(tumor, normal, config)
+  )
+
+  expr_cna <- "Using external CNA data"
+  expr <- "pipeline_tumor_normal complete"
+
+  testthat::expect_true(
+    stringr::str_detect(stdout, expr_cna)
+  )
+
+  testthat::expect_true(
+    stringr::str_detect(stdout, expr)
+  )
+})
