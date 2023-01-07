1cbcefafc5ac1b82411ddc51ff2b5db89bc0c4a6
Nana Mensah <Nana.mensah@crick.ac.uk>
2023-01-07 16:43:15 +0000

    Add function to run CAMDAC on BED file input

---
+++ tests/testthat/test-segments_bed.R
@@ -0,0 +1,30 @@
+test_that("allele counting regions can be read from BED file", {
+  # Load test sample and config
+  config <- create_camdac_config(
+    # camdac_refs = "./camdac_refs", # Optional: Pass the location of pipeline files
+    outdir = "./wgbs_test", # Path for outputs
+    bsseq = "wgbs", # WGBS
+    build = "hg38", # Reference
+    bsseq_lib = "pe", # Paired end
+    n_cores = 10 # Cores for parallel processing
+  )
+
+  tumor <- create_camdac_sample(
+    patient_id = "P1",
+    patient_sex = "XX",
+    sample_id = "T",
+    sample_type = "tumour",
+    # Load segments from BED file
+    bam_file = system.file("extdata", "NA18939_test_v3.bam", package = "CAMDAC"),
+    segments_bed = system.file("testdata", "test_segments.bed", package = "CAMDAC")
+  )
+
+  cmain_count_alleles(tumor, config)
+  ac_file <- build_output_name(tumor, config, "allele_counts")
+
+  testthat::expect_true(
+    fs::file_exists(
+      ac_file
+    )
+  )
+})
