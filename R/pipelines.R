551872c3e5cf1d16668b2ad38f85885125b37f6d
Nana Mensah <Nana.mensah@crick.ac.uk>
2023-01-14 12:02:01 +0000

    Refactor main pipeline

--- R/pipelines.R
+++ R/pipelines.R
@@ -6,33 +6,25 @@
 #' @param normal. Normal sample data built with `CamSample()`.
 #' @param config. Configuration built with `CamConfig()`.
 #' @export
-pipeline_tumor_normal <- function(tumor, normal, config) {
+pipeline <- function(tumor, germline = NULL, infiltrates = NULL, origin = NULL, config) {
   # Log
-  loginfo("CAMDAC:::pipeline_tumor_normal start for %s", tumor$patient_id)
+  loginfo("CAMDAC:::pipeline start for %s", tumor$patient_id)
 
-  # Count SNP and CpG alleles
-  cmain_count_alleles(tumor, config)
-  cmain_count_alleles(normal, config)
+  # Preprocess CpG, SNP and methylation data for all samples
+  preprocess(
+    list(tumor, germline, infiltrates, origin),
+    config
+  )
 
-  # TODO: Refactor into a CMAIN function that accepts battenberg or ascat as input
-  # If no allele-specific segments are given, call them
-  # Or refit if cna object contains purity and ploidy values alone
-  if (is.null(tumor$cna$ascna)) {
-    # Call copy number abberations using battenberg
-    cmain_make_snp_profiles(tumor, normal, config)
-    cmain_run_battenberg(tumor, normal, config)
-    tumor <- load_cna_data(tumor, config, "battenberg")
-  } else {
-    loginfo("Using external CNA data provided for %s", tumor$patient_id)
-  }
+  # Combine tumor-germline SNPs and call CNAs
+  cmain_bind_snps(tumor, germline, config)
+  cmain_call_cna(tumor, germline, config)
 
-  # Deconvolve methylation rates
-  cmain_make_methylation_profile(tumor, config)
-  cmain_make_methylation_profile(normal, config)
-  cmain_deconvolve_methylation(tumor, normal, config)
+  # Run deconvolution
+  cmain_deconvolve_methylation(tumor, infiltrates, config)
 
   # Call differential methylation
-  cmain_call_dmps(tumor, normal, config)
+  cmain_call_dmps(tumor, origin, config)
   cmain_call_dmrs(tumor, config)
 
   # Log
@@ -74,16 +66,19 @@ pipeline_tumor_panel <- function(tumor, infiltrates = normal, cell_of_origin = n
   loginfo("CAMDAC:::pipeline_tumor_panel complete for %s", tumor$patient_id)
 }
 
-validate_pipeline_tumor_panel <- function(tumor, normal, config) {
-  # Check if tumor has CNA computed
-  # TODO: Add tumor CNA attachment function as with methylation
-  if (is.null(tumor$cna$ascna)) {
-    stop("Tumor CNA data not found. Please add cna object to tumor `attach_cna_data()`.")
-  }
+#' Preprocess a list of CamSample objects for analysis
+#' @param sample_list. List of CamSample objects.
+#' @param config. CamConfig object.
+#' @export
+preprocess <- function(sample_list, config) {
+  for (s in sample_list) {
+    # Count SNP and CpG alleles
+    cmain_count_alleles(s, config)
 
-  # Check if normal has methylation object
-  normal_meth_exists <- file.exists(get_fpath(normal, config, "methylation"))
-  if (!normal_meth_exists) {
-    stop("Normal methylation data not found. Please run `attach_methylation_panel()` first.")
+    # Prepare SNP data for CNA calling
+    cmain_make_snps(s, config)
+
+    # Format methylation rates for deconvolution
+    cmain_make_methylation_profile(s, config)
   }
 }
