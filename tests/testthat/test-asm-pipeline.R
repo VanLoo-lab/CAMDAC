9253777b62bfa12b81f87deca45558c3d94bd207
Nana Mensah <nana.mensah@crick.ac.uk>
2023-03-24 12:38:19 +0000

    Write ASM pipeline test for BAM files

---
+++ tests/testthat/test-asm-pipeline.R
@@ -0,0 +1,16 @@
+test_that("ASM pipeline runs", {
+  b_tumor <- system.file("testdata", "tumor.bam", package = "CAMDAC")
+  b_normal <- system.file("testdata", "normal.bam", package = "CAMDAC")
+
+  tumor <- CamSample(id="T", sex="XY", bam=b_tumor)
+  normal <- CamSample(id="N", sex="XY", bam=b_normal)
+  config <- CamConfig(outdir="./result_asm_full", bsseq="wgbs", lib="pe", build="hg38", n_cores=10)
+
+  asm_pipeline(
+      tumor=tumor,
+      germline=normal,
+      infiltrates=normal,
+      origin=normal,
+      config=config
+  )
+})
