320c0252bed34d588476590e55bd14327a44b796
Nana Mensah <nana.mensah@crick.ac.uk>
2023-03-26 07:54:21 +0100

    Add min_cov for test data in ASM test

--- tests/testthat/test-asm-pipeline.R
+++ tests/testthat/test-asm-pipeline.R
@@ -4,7 +4,8 @@ test_that("ASM pipeline runs", {
 
   tumor <- CamSample(id="T", sex="XY", bam=b_tumor)
   normal <- CamSample(id="N", sex="XY", bam=b_normal)
-  config <- CamConfig(outdir="./result_asm_full", bsseq="wgbs", lib="pe", build="hg38", n_cores=10)
+  config <- CamConfig(
+    outdir="./result_asm_full", bsseq="wgbs", lib="pe", build="hg38", n_cores=10, min_cov=1)
 
   asm_pipeline(
       tumor=tumor,
