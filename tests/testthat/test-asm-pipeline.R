c797f7bfc83fc9fa117c92b42459b5f8b0c179eb
Nana Mensah <nana.mensah@crick.ac.uk>
2023-03-28 18:12:34 +0100

    Use ASCAT in ASM pipeline test

--- tests/testthat/test-asm-pipeline.R
+++ tests/testthat/test-asm-pipeline.R
@@ -5,7 +5,8 @@ test_that("ASM pipeline runs", {
   tumor <- CamSample(id="T", sex="XY", bam=b_tumor)
   normal <- CamSample(id="N", sex="XY", bam=b_normal)
   config <- CamConfig(
-    outdir="./result_asm_full", bsseq="wgbs", lib="pe", build="hg38", n_cores=10, min_cov=1)
+    outdir="./result_asm_full", bsseq="wgbs", lib="pe",
+    build="hg38", n_cores=3, min_cov=1, cna_caller="ascat")
 
   asm_pipeline(
       tumor=tumor,
