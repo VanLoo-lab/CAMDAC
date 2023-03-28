0e774a7e25f6286297f5e4d8aea066aae1178ae7
Nana Mensah <Nana.mensah@crick.ac.uk>
2023-03-28 18:35:57 +0100

    Split asm_meth_cna functions for merging meth and cna objects

--- R/asm_meth_cna.R
+++ R/asm_meth_cna.R
@@ -1,8 +1,4 @@
-overlap_meth_cna <- function(asm_meth, cna, hap_stats, phase_map) {
-    # Format data for merge
-    setkey(asm_meth, chrom, start, end)
-    setkey(cna, chrom, start, end)
-
+merge_asm_hap <- function(asm_meth, hap_stats, phase_map) {
     # Annotate BAF of SNP for each phased CpG
     baf <- hap_stats[, .(
         hap_POS, hap_id, hap_BAF, hap_reads,
@@ -10,10 +6,21 @@ overlap_meth_cna <- function(asm_meth, cna, hap_stats, phase_map) {
     )]
     phase <- unique(phase_map[, .(chrom, start, end, hap_id)])
     phase$chrom <- stringr::str_replace(phase$chrom, "chr", "")
-    phase <- merge(phase, baf, all.y = T, by = "hap_id") # all.y required?
+    phase <- merge(phase, baf, all.y = T, by = "hap_id")
+
+    # Merge methylation and haplotype stats
+    setkey(phase, chrom, start, end)
+    res <- merge(asm_meth, phase, all.x = T, by = c("chrom", "start", "end"))
+    return(res)
+}
+
+overlap_meth_cna <- function(asm_hap, cna) {
+    # Format data for merge
+    setkey(asm_hap, chrom, start, end)
+    setkey(cna, chrom, start, end)
 
     # Merge methylation and cna
-    amc <- data.table::foverlaps(asm_meth, cna, mult = "first", nomatch = NA)
+    amc <- data.table::foverlaps(asm_hap, cna, mult = "first", nomatch = NA)
     amc$start <- amc$cna_start
     amc$end <- amc$cna_end
     amc$start <- amc$i.start
@@ -22,11 +29,7 @@ overlap_meth_cna <- function(asm_meth, cna, hap_stats, phase_map) {
     amc$i.end <- NULL
     setkey(amc, chrom, start, end)
 
-    # Merge meth_cna and BAF of phased SNPs, along with cg-phase annotation
-    setkey(phase, chrom, start, end)
-    res <- merge(amc, phase, all.x = T, by = c("chrom", "start", "end"))
-
-    return(res)
+    return(amc)
 }
 
 assign_asm_cna <- function(ol) {
@@ -40,12 +43,12 @@ assign_asm_cna <- function(ol) {
         )
     ]
 
-    ol[ hap_BAF <= 0.5, `:=`(
+    ol[hap_BAF <= 0.5, `:=`(
         ref_CN = nA,
         alt_CN = nB
     )]
 
-    ol[ hap_BAF > 0.5, `:=`(
+    ol[hap_BAF > 0.5, `:=`(
         ref_CN = nB,
         alt_CN = nA
     )]
