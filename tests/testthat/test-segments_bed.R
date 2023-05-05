a9f72b5b71e47d06e97f375dcf9176ed16a65fd6
Nana Mensah <Nana.mensah@crick.ac.uk>
2023-05-05 20:31:10 +0100

    Update segment test with cleanup code

--- tests/testthat/test-segments_bed.R
+++ tests/testthat/test-segments_bed.R
@@ -1,5 +1,4 @@
 test_that("allele counting regions can be read from BED file", {
-  # Est. 5 minutes
   bam <- system.file("testdata", "tumor.bam", package = "CAMDAC")
   seg_regions <- system.file("testdata", "test_segments.bed", package = "CAMDAC")
 
@@ -7,6 +6,8 @@ test_that("allele counting regions can be read from BED file", {
   config_t <- config
   config_t$outdir <- "./result_test_segments"
   config_t$regions <- seg_regions
+  config_t$overwrite <- TRUE
+  withr::defer(fs::dir_delete(config_t$outdir))
 
   # Run allele counting
   tumor <- CamSample(id = "T", sex = "XY", bam = bam)
@@ -17,5 +18,20 @@ test_that("allele counting regions can be read from BED file", {
     fs::file_exists(
       ac_file
     )
+  )
+
+  # Check that allele counts regions do not extend beyond the segments given
+  dt <- fread_chrom(ac_file)
+  regs <- data.table::fread(seg_regions)
+  names(regs) <- c("chrom", "start", "end")
+
+  testthat::expect_equal(
+    unique(dt$CHR),
+    unique(regs$chrom)
+  )
+
+  all_within <- max(dt$start) <= max(regs$end) && min(dt$end) >= min(regs$start)
+  testthat::expect_true(
+    all_within
   )
 })
