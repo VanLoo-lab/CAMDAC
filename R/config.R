ab92e4e8ba162b987e8308a20b4383cae963dff8
Nana Mensah <Nana.mensah@crick.ac.uk>
2023-05-29 02:29:54 +0100

    Remove hg19 battenberg warning

--- R/config.R
+++ R/config.R
@@ -41,12 +41,6 @@ CamConfig <- function(outdir, bsseq, lib, build, n_cores = 1, regions = NULL, re
   # Set camdac references if not they do not exist
   refs <- ifelse(is.null(refs), pipeline_files(), fs::path_real(refs))
 
-  # Battenberg reference files are not available for hg19. Set ASCAT as caller in this case.
-  if (build == "hg19" && cna_caller == "battenberg") {
-    logging::logwarn("Battenberg.m not yet implemented for hg19 in CAMDAC. Setting ASCAT as CNA caller.")
-    cna_caller <- "ascat"
-  }
-
   # If using battenberg, validate that java is available and set beagle jar
   if (cna_caller == "battenberg") {
     check_java()
@@ -64,6 +58,7 @@ CamConfig <- function(outdir, bsseq, lib, build, n_cores = 1, regions = NULL, re
 
   # If using rrbs, CNA caller must be ASCAT
   if (bsseq == "rrbs") {
+    logwarn("CNA caller set to ASCAT for RRBS data")
     cna_caller <- "ascat"
   }
 
