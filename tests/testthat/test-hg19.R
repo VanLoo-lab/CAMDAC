dcdd63ead85cf3b3fa2d9d3219fe742b4acc76f3
Nana Mensah <Nana.mensah@crick.ac.uk>
2023-05-29 16:56:29 +0100

    Add hg19 pipeline test using battenberg and test bams

--- tests/testthat/test-hg19.R
+++ tests/testthat/test-hg19.R
@@ -1,4 +1,4 @@
-test_that("CAMDAC runs on hg19 samples samples", {
+test_that("CAMDAC runs with battenberg in hg19 mode", {
     # CNA caller test config
     config_hg19 <- CamConfig(
         outdir = "./result_hg19_pe",
@@ -9,17 +9,8 @@ test_that("CAMDAC runs on hg19 samples samples", {
         n_cores = 10,
         min_cov = 1 # Required to capture sufficient SNPs from test
     )
+    config_hg19$regions = system.file("testdata", "test_wgbs_segments.bed", package = "CAMDAC")
 
-    # Test full pipeline on hg19 
-    ## N.B. : Regions only returns 19 sites. Need to rebuild hg19 test BAM
-    ## Overwrite regions for full test
-    ## regions <- system.file("testdata", "test_wgbs_segments_hg19_v2.bed", package = "CAMDAC")
-    ## config_hg19$regions = regions
-
-    tumor <- CamSample(id = "T", sex = "XY",
-        bam = system.file("testdata", "tumor_hg19.bam", package = "CAMDAC"))
-    normal <- CamSample(id = "N", sex = "XY",
-        bam = system.file("testdata", "normal_hg19.bam", package = "CAMDAC"))  
     stdout <- testthat::capture_output(
         pipeline(tumor, germline = normal, infiltrates = normal, origin = normal, config_hg19)
     )
