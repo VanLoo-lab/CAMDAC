bd8ffd1b0e6ba7d2ca502a347008b78cbcd3bfc2
Nana Mensah <nana.mensah@crick.ac.uk>
2025-05-07 12:06:32 +0100

    refactor: Create pipeline functions to dispatch to RRBS or WGBS respectively

--- R/pipeline.R
+++ R/pipeline.R
@@ -1,6 +1,21 @@
-#' CAMDAC tumor-normal pipeline
-#'
-#' Run CAMDAC analysis on a bulk tumor and patient-matched tissue-matched tumor-adjacent normal sample.
+#' CAMDAC analysis pipeline
+#' 
+#' @param tumor Tumor `CamSample` object for deconvultion.
+#' @param germline Patient-matched normal `CamSample` object. May be NULL if `tumor` has CNA calls already.
+#' @param infiltrates Normal `CamSample` as a proxy for infiltrating normal methylation.
+#' @param origin Normal `CamSample` representing cell of origin for tumor-normal differential methylation.
+#' @param config Configuration built with `CamConfig()`.
+pipeline <- function(tumor, germline, infiltrates, origin, config) {
+  if (config$bsseq == "wgbs"){
+    pipeline_wgbs(tumor, germline, infiltrates, origin, config)
+  } else if (config$bsseq == "rrbs") {
+    pipeline_rrbs(tumor, germline, infiltrates, origin, config)
+  } else {
+    stop("Unsupported bsseq type. Please use 'wgbs' or 'rrbs'.")
+  }
+}
+
+#' Run CAMDAC WGBS analysis on a bulk tumor and patient-matched tissue-matched tumor-adjacent normal sample.
 #'
 #' @param tumor Tumor `CamSample` object for deconvultion.
 #' @param germline Patient-matched normal `CamSample` object. May be NULL if `tumor` has CNA calls already.
@@ -8,12 +23,12 @@
 #' @param origin Normal `CamSample` representing cell of origin for tumor-normal differential methylation.
 #' @param config Configuration built with `CamConfig()`.
 #' @export
-pipeline <- function(tumor, germline = NULL, infiltrates = NULL, origin = NULL, config) {
+pipeline_wgbs <- function(tumor, germline = NULL, infiltrates = NULL, origin = NULL, config) {
   # Log
   loginfo("CAMDAC:::pipeline start for %s", tumor$patient_id)
 
   # Preprocess CpG, SNP and methylation data for all samples
-  preprocess(
+  preprocess_wgbs(
     list(tumor, germline, infiltrates, origin),
     config
   )
@@ -37,7 +52,7 @@ pipeline <- function(tumor, germline = NULL, infiltrates = NULL, origin = NULL, 
 #' @param sample_list. List of CamSample objects.
 #' @param config. CamConfig object.
 #' @export
-preprocess <- function(sample_list, config) {
+preprocess_wgbs <- function(sample_list, config) {
   for (s in sample_list) {
     # Go to next part of loop if its null
     if (is.null(s)) {
@@ -53,4 +68,219 @@ preprocess <- function(sample_list, config) {
     # Format methylation rates for deconvolution
     cmain_make_methylation_profile(s, config)
   }
+}
+
+
+#' Call CAMDAC for a tumor and patient-matched normal sample
+#' 
+#' @param tumor Tumor `CamSample` object for deconvultion.
+#' @param germline Patient-matched normal `CamSample` object. May be NULL if `tumor` has CNA calls already.
+#' @param infiltrates Normal `CamSample` as a proxy for infiltrating normal methylation.
+#' @param origin Normal `CamSample` representing cell of origin for tumor-normal differential methylation.
+#' @param config Configuration built with `CamConfig()`.
+#' @export
+#'
+#' @param patient_id character. Patient identifier
+#' @param tumor_id character. Tumor sample identifier
+#' @param normal_id character. Normal sample identifier
+#' @param tumor_bam character. Full path to tumor bam file
+#' @param normal_bam character. Full path to normal bam file
+#' @param sex character. Patient sex: "XX" for female or "XY" for male
+#' @param path character. Full path to CAMDAC output directory
+#' @param pipeline_files character. Full path to parent directory containing CAMDAC pipeline_files
+#' @param build character. Genome build: "hg19" or "hg38"
+#' @param min_tumor integer. Minimum read filter for tumor samples
+#' @param min_normal integer. Minimum read filter for normal samples
+#' @param n_cores integer. Number of cores to use for parallel processing
+#' @param mq integer. Minimum mapping quality filter
+#' @export
+pipeline_rrbs <- function(tumor, germline, infiltrates, origin, config){
+
+  # Preprocess RRBS normal samples
+  for (s in list(germline, infiltrates, origin, config)){
+
+    # Go to next part of loop if its null
+    if (is.null(s)) {
+        next
+    }
+
+    preprocess_rrbs_normal(
+      patient_id = s$patient_id , sample_id = s$id, bam_file = s$bam,
+      min_tumor = 1, min_norm = config$min_normal_cov, mq = config$min_mapq,
+      sex = s$sex, path = config$outdir,
+      pipeline_files = config$refs, build = config$build,
+      n_cores = config$n_cores
+    )
+  }
+
+  # Main : Process RRBS tumour using the design from input files
+
+  # Setup
+  patient_id <- tumor$patient_id
+  sample_id <- tumor$id
+  bam_file <- tumor$bam
+  sex <- tumor$sex
+  path <- config$outdir
+  pipeline_files <- config$refs
+  build <- config$build
+  n_cores <- config$n_cores
+  min_tumor <- 1
+  min_normal <- config$min_normal_cov
+  mq <- config$min_mapq
+
+  # Define expected ac file
+  ac_file = file.path(
+    path, patient_id, "Allelecounts", sample_id,
+    paste0(patient_id, ".", sample_id, ".SNPs.CpGs.all.sorted.RData")
+  )
+
+  if (!file.exists(ac_file)) {
+    loginfo("CAMDAC:::preprocess_rrbs_tumor: %s:%s", patient_id, sample_id)
+    # Run allele counter for tumor sample
+    for (a in 1:25) {
+        get_allele_counts(
+            i = a, patient_id = patient_id, sample_id = sample_id,
+            sex = sex, bam_file = bam_file, mq = mq,
+            path, pipeline_files, build, n_cores, test = FALSE
+        )
+    }
+
+    # Merge allele counts
+    format_output(
+        patient_id, sample_id, sex, is_normal=FALSE, path, pipeline_files, build
+    )
+
+  } else {
+    loginfo("CAMDAC:::preprocess_rrbs_tumor: %s already exists, skipping counts.", ac_file)
+  }
+
+  # Create SNP files and run ASCAT (tumor)
+  loginfo("CAMDAC:::ASCAT.m Tumor")
+  run_ASCAT.m(
+      patient_id, sample_id, sex,
+      patient_matched_normal_id = germline$id,
+      path, pipeline_files, build,
+      min_normal, min_tumor,
+      n_cores, reference_panel_coverage = NULL
+  )
+
+  # Process methylation info for copy number profiling and plot summary.
+  loginfo("CAMDAC:::run_methylation_data_processing Tumor")
+  run_methylation_data_processing(
+      patient_id, sample_id,
+      normal_infiltrates_proxy_id = infiltrates$id,
+      normal_origin_proxy_id = origin$id,
+      path, min_normal, min_tumor, n_cores,
+      reference_panel_normal_infiltrates = NULL,
+      reference_panel_normal_origin = NULL
+  )
+
+  # Get purified methylation rate
+  loginfo("CAMDAC:::get_pure_tumour_methylation Tumor")
+  get_pure_tumour_methylation(
+      patient_id = patient_id, sample_id = sample_id, sex = sex,
+      normal_infiltrates_proxy_id = infiltrates$id,
+      path, pipeline_files, build,
+      n_cores, reseg = FALSE
+  )
+
+  # Get DMP and DMR calls
+  loginfo("CAMDAC:::get_differential_methylation Tumor")
+  get_differential_methylation(
+      patient_id = patient_id, sample_id = sample_id, sex = sex,
+      normal_origin_proxy_id = origin$id,
+      path, pipeline_files, build,
+      effect_size = 0.2, prob = 0.99,
+      min_DMP_counts_in_DMR = 5, min_consec_DMP_in_DMR = 4,
+      n_cores, reseg = FALSE, bulk = FALSE
+  )
+}
+
+preprocess_rrbs_normal <- function(patient_id, sample_id, bam_file, min_tumor,
+                              min_normal, mq, sex, path, pipeline_files, build, n_cores) {
+
+    # Define expected allele counts.
+    ac_file = file.path(
+      path, patient_id, "Allelecounts", sample_id,
+      paste0(patient_id, ".", sample_id, ".SNPs.CpGs.all.sorted.RData")
+    )
+
+    loginfo("CAMDAC:::preprocess_rrbs_normal: %s:%s", patient_id, sample_id)
+    if(!file.exists(ac_file)) {
+
+      # Run allele counter for normal sample
+      for (a in 1:25) {
+          get_allele_counts(
+              i = a, patient_id = patient_id, sample_id = sample_id, sex = sex, bam_file, mq = mq,
+              path, pipeline_files, build, n_cores, test = FALSE
+          )
+      }
+
+      # Merge allele counts
+      # Set normal status based on whether sample and normal ID match
+      normal_id = sample_id
+      is_normal <- ifelse(sample_id == normal_id, TRUE, FALSE)
+      format_output(
+          patient_id, sample_id, sex, is_normal, path, pipeline_files, build
+      )
+    } else {
+        loginfo("CAMDAC:::preprocess_rrbs_normal: %s already exists, skipping counts.", ac_file)
+    }
+
+    # Create SNP files (normal) or run ASCAT (tumor)
+    run_ASCAT.m(
+        patient_id, sample_id, sex,
+        patient_matched_normal_id = normal_id,
+        path, pipeline_files, build,
+        min_normal, min_tumor,
+        n_cores, reference_panel_coverage = NULL
+    )
+
+    # Process methylation info for copy number profiling and plot summary.
+    run_methylation_data_processing(
+        patient_id, sample_id,
+        normal_infiltrates_proxy_id = normal_id,
+        normal_origin_proxy_id = normal_id,
+        path, min_normal, min_tumor, n_cores,
+        reference_panel_normal_infiltrates = NULL,
+        reference_panel_normal_origin = NULL
+    )
+                              }
+
+
+preprocess_rrbs_tumor <- function(patient_id, sample_id, normal_id, bam_file, min_tumor,
+                              min_normal, mq, sex, path, pipeline_files, build, n_cores) {
+    # Run allele counter for normal sample
+    for (a in 1:25) {
+        get_allele_counts(
+            i = a, patient_id = patient_id, sample_id = sample_id, sex = sex, bam_file, mq = mq,
+            path, pipeline_files, build, n_cores, test = FALSE
+        )
+    }
+
+    # Merge allele counts
+    # Set normal status based on whether sample and normal ID match
+    is_normal <- ifelse(sample_id == normal_id, TRUE, FALSE)
+    format_output(
+        patient_id, sample_id, sex, is_normal, path, pipeline_files, build
+    )
+
+    # Create SNP files (normal) or run ASCAT (tumor)
+    run_ASCAT.m(
+        patient_id, sample_id, sex,
+        patient_matched_normal_id = normal_id,
+        path, pipeline_files, build,
+        min_normal, min_tumor,
+        n_cores, reference_panel_coverage = NULL
+    )
+
+    # Process methylation info for copy number profiling and plot summary.
+    run_methylation_data_processing(
+        patient_id, sample_id,
+        normal_infiltrates_proxy_id = normal_id,
+        normal_origin_proxy_id = normal_id,
+        path, min_normal, min_tumor, n_cores,
+        reference_panel_normal_infiltrates = NULL,
+        reference_panel_normal_origin = NULL
+    )
 }
